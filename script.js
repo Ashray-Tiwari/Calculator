f413981e1a4c592464b13be861c46423da07d96d
Ashray-Tiwari <90513905+Ashray-Tiwari@users.noreply.github.com>
2023-01-02 19:36:56 +0530

    1st changes

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ buttons.map( button => {
         }
     })
 }) 
+
+document.addEventListener('keyup', (event) => {
+    var name = event.key;
+    switch(name){
+        case 'C':
+            display.innerText = '';
+            break;
+        case 'Backspace':
+            if(display.innerText){
+                display.innerText = display.innerText.slice(0,-1);
+            }
+            break;
+        case '=':
+            try{
+                display.innerText = eval(display.innerText);
+            }catch{
+                display.innerText = 'Error';
+            }
+            break;
+        case 'Enter':
+            try{
+                display.innerText = eval(display.innerText);
+            }catch{
+                display.innerText = 'Error';
+            }
+            break;
+        
+        default:
+            if(name == '1' || name == '2' || name == '3' || name == '4' || name == '5' || name == '6' || name == '7' || name == '8' || name == '9' || name == '+' || name == '-' || name == '*' || name == '/' || name == ')' || name == '('  )
+            display.innerText+=name;
+    }
+  }, false);
